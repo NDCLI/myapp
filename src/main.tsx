a04f465ef272c9c3cf2e72a4e4b0741121e43939
NDCLI <nguyenkimhoa004@gmail.com>
2026-07-17 17:09:51 +0700

    Add PostHog configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+posthog.init('phc_qspicXaSsvbdvfV74uZfS6Gc8hQN5wiX6g6cvufeN7p8', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
